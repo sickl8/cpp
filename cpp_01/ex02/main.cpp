49d700a2dbae7ea3d4ae6c50a0274eb4823797e7
Ilyass Saadi <isaadi@e0r4p6.1337.ma>
2021-09-21 18:13:57 +0100

    some changes

--- cpp_01/ex02/main.cpp
+++ cpp_01/ex02/main.cpp
@@ -1,25 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: isaadi <isaadi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/19 17:15:25 by isaadi            #+#    #+#             */
-/*   Updated: 2021/09/21 17:53:16 by isaadi           ###   ########.fr       */
+/*   Created: 2021/09/21 17:39:21 by isaadi            #+#    #+#             */
+/*   Updated: 2021/09/21 17:45:32 by isaadi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <iostream>
 
-class Zombie {
-	public:
-		void	announce(void);
-		Zombie(std::string _name);
-		~Zombie();
-		static void	randomChump(std::string name);
-		static Zombie	*newZombie(std::string name);
-	private:
-		std::string name;
-};
+int	main() {
+	std::string orig = "HI THIS IS BRAIN";
+
+	std::string *stringPTR = &orig;
+	std::string &stringREF = orig;
+	
+	
+	std::cout << &orig << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+	
+}
--- cpp_01/ex03/Weapon.hpp
+++ cpp_01/ex03/Weapon.hpp
@@ -1,25 +1,22 @@
+#ifndef __WEAPON_HPP__
+# define __WEAPON_HPP__
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: isaadi <isaadi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/19 17:15:25 by isaadi            #+#    #+#             */
-/*   Updated: 2021/09/21 17:53:16 by isaadi           ###   ########.fr       */
+/*   Created: 2021/09/21 17:50:15 by isaadi            #+#    #+#             */
+/*   Updated: 2021/09/21 18:12:33 by isaadi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <iostream>
 
-class Zombie {
-	public:
-		void	announce(void);
-		Zombie(std::string _name);
-		~Zombie();
-		static void	randomChump(std::string name);
-		static Zombie	*newZombie(std::string name);
+class Weapon {
 	private:
-		std::string name;
+		std::string type;
 };
+
+#endif
