25d7faefbb70c535ec68b49dc4babda939fcf903
Ilyass Saadi <isaadi@e1r7p6.1337.ma>
2021-11-15 00:52:14 +0100

    small changes

--- cpp_04/ex00/Animal.cpp
+++ cpp_04/ex00/Animal.cpp
@@ -1,22 +1,14 @@
-#include "Animal.hpp"
+#include"Animal.hpp"
 
 #define endl std::endl
 
 #define cout std::cout
-
-Animal::Animal() {
-	this->type = "";
-	cout << "Animal created" << endl;
-}
 
 Animal::Animal(std::string _type) {
 	this->type = _type;
 	cout << "Animal created" << endl;
 }
 
-Animal::~Animal() {
-	cout << "Animal deleted" << endl;
-}
 
 void	Animal::makeSound() const {
 	cout << "I can't make a sound since I don't know what I am" << endl;
@@ -25,3 +17,17 @@ void	Animal::makeSound() const {
 std::string const&Animal::getType() const {
 	return this->type;
 }
+
+
+Animal::Animal() {
+	this->type = "";
+	cout << "Animal created" << endl;
+}
+
+Animal::  ~    Animal() {
+	cout << "Animal deleted" << endl;
+}
+
+Animal	&Animal::operator=(const Animal &ref) {}
+
+Animal::Animal(const Animal &ref) {}
--- cpp_04/ex00/Animal.hpp
+++ cpp_04/ex00/Animal.hpp
@@ -1,12 +1,15 @@
 #ifndef __ANIMAL_HPP__
 # define __ANIMAL_HPP__
 
+
 # include <iostream>
 
-class Animal {
+class Animal{
 	public:
 		Animal();
 		~Animal();
+		Animal(const Animal &ref);
+		Animal	&operator=(const Animal &ref);
 		Animal(std::string _type);
 		virtual void makeSound() const;
 		std::string const &getType() const;
