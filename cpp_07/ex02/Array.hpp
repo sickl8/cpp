724e6ca838f778bc926d851246dbd893bf805408
Ilyass Saadi <isaadi@e1r7p4.1337.ma>
2021-12-19 02:02:51 +0100

    ex02/cpp_07

---
+++ cpp_07/ex02/Array.hpp
@@ -0,0 +1,53 @@
+#ifndef __ARRAY_HPP__
+# define __ARRAY_HPP__
+
+
+template <typename T>
+class Array {
+	public:
+		~Array() {
+			delete array;
+		}
+		Array(): excep("index out of bounds of Array") {
+			array = nullptr;
+			_size = 0;
+		};
+		Array(const Array &ref): excep("index out of bounds of Array") {
+			array = new T[ref._size];
+			_size = ref._size;
+			for (unsigned int i = 0; i < _size; i++) {
+				array[i] = ref.array[i];
+			}
+		}
+		Array(unsigned int n): excep("index out of bounds of Array") {
+			array = new T[n];
+			_size = n;
+			for (unsigned int i = 0; i < _size; i++) {
+				array[i] = static_cast<T>(0);
+			}
+		}
+		Array	&operator=(const Array &ref) {
+			delete array;
+			array = new T[ref._size];
+			_size = ref._size;
+			for (unsigned int i = 0; i < _size; i++) {
+				array[i] = ref.array[i];
+			}
+		}
+		T		&operator[](long index) {
+			if (index < 0 || index >= _size) {
+				throw excep;
+			}
+			return array[index];
+		}
+		unsigned int	size() {
+			return _size;
+		}
+	private:
+		T					*array;
+		unsigned int		_size;
+		std::out_of_range	excep;
+
+};
+
+#endif
