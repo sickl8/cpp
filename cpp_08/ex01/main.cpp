1ef0a43d97ed7c07995723276264ac3fe9be4071
Ilyass Saadi <isaadi@e1r6p4.1337.ma>
2021-12-23 04:51:20 +0100

    ex01/cpp_08

--- cpp_08/ex01/main.cpp
+++ cpp_08/ex01/main.cpp
@@ -106,4 +106,14 @@ int		main(
 			std::cout << "throwed as expected, what() == " << e.what() << std::endl;
 		}
 	}
+	{
+		Span sp = Span(5);
+		sp.addNumber(5);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
 }
